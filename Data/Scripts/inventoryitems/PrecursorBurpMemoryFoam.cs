dab239eff83c8b0487709cb35a6b0f01e88f8461
Nova <mariofluddd@gmail.com>
2021-09-13 14:52:16 -0300

    Many changes - Made Memory Foam give you infinite suit Oxygen, Hydrogen, and Energy (Health doesn't work for some reason) - Changed the icon of Memory Foam from Small Steel Tube to Canvas - Changed the item model of Memory Foam from Small Steel Tube to Canvas - Changed the item model of the Handheld Energy Core to be a smallship battery - Added 'Precursor' to the Id's of Feo'r and Memory Foam to prevent any possible conflicts (mostly with the memory foam) - Moved the word 'Zei'a' to the front of the Energy Core and missiles to make them easier to find when spawning - Removed a copy of Keen's seats file - Fixed the subtype Id's for the warheads - Made the Zei'a Remote Control take over 115 days to grind down so it is very very hard to steal most Zei'a ships - Moved the Zei'a components in everything so that you need them to place even the frame of a Zei'a block - Added Zei'a component requirements to the smallship refinery so any old shmuck can't build it - Renamed the 'VB' seat to 'Offset' - Made the Zei'a ship weapons and tools able to be equipped like normal weapons and tools - Made Block Variant Groups to the Spotlights, Thrusters, Ship Tools, and Warheads - Made the radius of Zei'a Missiles the same as the Alpha Warhead because like 3 different people thought it was dissapointing - Maybe fixed the Refinery and Assembler from making negative stuff while using upgrade modules

---
+++ Data/Scripts/inventoryitems/PrecursorBurpMemoryFoam.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.Common;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Common.ObjectBuilders.Definitions;
+using Sandbox.Definitions;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+
+namespace PrecursorBurpMemoryFoam{
+	
+	[MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
+		
+	public class HealthEnergyConsumables : MySessionComponentBase{
+		
+		int tickTimer = 0;
+		bool scriptInit = false;
+		 
+		MyObjectBuilder_PhysicalGunObject energyHalf;
+		
+		public override void UpdateBeforeSimulation(){
+			if(scriptInit == false){
+				scriptInit = true;
+				var definitionId = new MyDefinitionId(typeof(MyObjectBuilder_PhysicalGunObject), "PrecursorBurpMemoryFoam");
+				energyHalf = (MyObjectBuilder_PhysicalGunObject)MyObjectBuilderSerializer.CreateNewObject(definitionId);
+			}
+			tickTimer++;
+			if(tickTimer < 180){
+				return;
+			}
+			tickTimer = 0;
+			var playerList = new List<IMyPlayer>();
+			MyAPIGateway.Players.GetPlayers(playerList);
+
+			if(playerList.Count == 0){
+				return;
+			}
+
+			foreach(var player in playerList){
+				if(player.IsBot == true){
+					continue;
+				}
+
+				if(player.Character == null){
+					continue;
+				}
+				var health = MyVisualScriptLogicProvider.GetPlayersHealth(player.IdentityId); //This doesn't work
+				var oxygen = MyVisualScriptLogicProvider.GetPlayersOxygenLevel(player.IdentityId);
+                var energy = MyVisualScriptLogicProvider.GetPlayersEnergyLevel(player.IdentityId);
+				var hydrogen = MyVisualScriptLogicProvider.GetPlayersHydrogenLevel(player.IdentityId);
+
+				if(health < 0.90f){
+					var Inv = player.Character.GetInventory();
+					if(Inv.ContainItems(1, energyHalf) == true){
+						MyVisualScriptLogicProvider.SetPlayersHealth	(player.IdentityId,1f);
+					}
+				}
+
+				if(oxygen < 0.90f){
+					var Inv = player.Character.GetInventory();
+					if(Inv.ContainItems(1, energyHalf) == true){
+						MyVisualScriptLogicProvider.SetPlayersOxygenLevel(player.IdentityId,1f);
+					}
+				}
+
+                if(energy < 0.90f){
+					var Inv = player.Character.GetInventory();
+					if(Inv.ContainItems(1, energyHalf) == true){
+						MyVisualScriptLogicProvider.SetPlayersEnergyLevel(player.IdentityId,1f);
+					}
+				}
+
+				if(hydrogen < 0.90f){
+					var Inv = player.Character.GetInventory();
+					if(Inv.ContainItems(1, energyHalf) == true){
+						MyVisualScriptLogicProvider.SetPlayersHydrogenLevel(player.IdentityId,1f);
+					}
+				}
+
+			}
+		}
+	}
+}
